ab1f66816f3ee7a73cdf6afd3bf442a6b6bb675c
鲍洲 <107630221@qq.com>
2018-10-03 11:28:51 +0800

    Initial commit

---
+++ MyBatisGenerator/src/main/java/com/bzcoder/test/entity/Company.java
@@ -0,0 +1,89 @@
+package com.bzcoder.test.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author BaoZhou
+ * @since 2018-10-03
+ */
+@TableName("tbl_company")
+public class Company extends Model<Company> {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String owner;
+
+    private String location;
+
+    private Integer status;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    @Override
+    protected Serializable pkVal() {
+        return this.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+        "id=" + id +
+        ", name=" + name +
+        ", owner=" + owner +
+        ", location=" + location +
+        ", status=" + status +
+        "}";
+    }
+}
---
+++ MyBatisGenerator/src/main/java/com/bzcoder/test/entity/Employee.java
@@ -0,0 +1,89 @@
+package com.bzcoder.test.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author BaoZhou
+ * @since 2018-10-03
+ */
+@TableName("tbl_employee")
+public class Employee extends Model<Employee> {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    private String lastName;
+
+    private String email;
+
+    private String gender;
+
+    private Integer age;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    protected Serializable pkVal() {
+        return this.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+        "id=" + id +
+        ", lastName=" + lastName +
+        ", email=" + email +
+        ", gender=" + gender +
+        ", age=" + age +
+        "}";
+    }
+}
